21982c84f5c30344f0d3ec1a596905f07bf0fa86
UTOXAS <abdullah621@outlook.com>
2025-03-15 08:20:59 +0200

    reverted to default MainActivity.kt

--- example/android/app/src/main/kotlin/com/example/flutter_yt_dlp_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/flutter_yt_dlp_example/MainActivity.kt
@@ -1,18 +1,6 @@
 package com.example.flutter_yt_dlp_example
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.RenderMode // Correct import for RenderMode
-import io.flutter.embedding.engine.FlutterEngine
-import android.content.Context
 
 class MainActivity : FlutterActivity() {
-    override fun getRenderMode(): RenderMode {
-        // Use texture mode instead of surface mode
-        return RenderMode.texture
-    }
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        // No additional configuration needed here for now
-    }
 }
